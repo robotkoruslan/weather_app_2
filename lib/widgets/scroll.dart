65c155c886d7e3c06b306f3aec864eee5075eaa1
Ruslan Robotko <robotko.ruslan@gmail.com>
2021-03-01 21:20:14 +0200

    Add loading

--- lib/widgets/scroll.dart
+++ lib/widgets/scroll.dart
@@ -7,23 +7,35 @@ import 'package:weather_app_2/widgets/start_message.dart';
 class WeatherScroll extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return (context.watch<WeatherApi>().getLocation == null ||
-            context.watch<WeatherApi>().getIsLoading)
+    return (context.watch<WeatherApi>().getLocation == null)
         ? startText()
         : SingleChildScrollView(
             scrollDirection: Axis.horizontal,
-            child: Row(
-              children: <Widget>[
-                for (var i = 0; i < 7; i++)
-                  ForecastElement(
-                      i + 1,
-                      context.watch<WeatherApi>().getabbreviationForecast2[i],
-                      context.watch<WeatherApi>().getMinTemperatureForecast2[i],
-                      context
-                          .watch<WeatherApi>()
-                          .getmaxTemperatureForecast2[i]),
-              ],
-            ),
+            child: (context.watch<WeatherApi>().getIsLoading)
+                ? Container(
+                    padding: EdgeInsets.all(40),
+                    child: Text(
+                      'Loading... ',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 30, color: Colors.white),
+                    ),
+                  )
+                : Row(
+                    children: <Widget>[
+                      for (var i = 0; i < 7; i++)
+                        ForecastElement(
+                            i + 1,
+                            context
+                                .watch<WeatherApi>()
+                                .getabbreviationForecast2[i],
+                            context
+                                .watch<WeatherApi>()
+                                .getMinTemperatureForecast2[i],
+                            context
+                                .watch<WeatherApi>()
+                                .getmaxTemperatureForecast2[i]),
+                    ],
+                  ),
           );
   }
 }
