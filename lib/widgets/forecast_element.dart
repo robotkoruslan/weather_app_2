cd71f4907ed7a38319da18c9e251e8d17d79de89
Ruslan Robotko <robotko.ruslan@gmail.com>
2021-03-03 23:22:49 +0200

    rename

--- lib/widgets/forecast_element.dart
+++ lib/widgets/forecast_element.dart
@@ -2,19 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 class ForecastElement extends StatelessWidget {
-  final int daysFromNow;
-  final String abbreviation;
-  final int minTemperature;
-  final int maxTemperature;
+  final int _daysFromNow;
+  final String _abbreviation;
+  final int _minTemperature;
+  final int _maxTemperature;
 
-  const ForecastElement(this.daysFromNow, this.abbreviation,
-      this.minTemperature, this.maxTemperature);
+  const ForecastElement(this._daysFromNow, this._abbreviation,
+      this._minTemperature, this._maxTemperature);
 
   @override
   Widget build(BuildContext context) {
     final now = DateTime.now();
-    final oneDayFromNow = now.add(Duration(days: daysFromNow));
-    final minTemperature2 = this.minTemperature;
+    final oneDayFromNow = now.add(Duration(days: _daysFromNow));
+
     return Padding(
       padding: const EdgeInsets.only(left: 16),
       child: Container(
@@ -38,17 +38,17 @@ class ForecastElement extends StatelessWidget {
                 padding: const EdgeInsets.only(top: 16, bottom: 16),
                 child: Image.network(
                   'https://www.metaweather.com/static/img/weather/png/' +
-                      abbreviation +
+                      _abbreviation +
                       '.png',
                   width: 50,
                 ),
               ),
               Text(
-                'High: ' + '$maxTemperature' + ' °C',
+                'High: ' + '$_maxTemperature' + ' °C',
                 style: const TextStyle(color: Colors.white, fontSize: 20),
               ),
               Text(
-                'Low: ' + '$minTemperature2' + ' °C',
+                'Low: ' + '$_minTemperature' + ' °C',
                 style: const TextStyle(color: Colors.white, fontSize: 20),
               ),
             ],
